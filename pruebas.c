a21924fa61cea1557b9f7fab325b4e269287ac58
Luca Lazcano <lazcanoluca@gmail.com>
2022-06-09 00:19:34 -0300

    xfghjt

--- pruebas.c
+++ pruebas.c
@@ -1,6 +1,8 @@
 #include "src/hash.h"
 #include "pa2m.h"
 
+// void 
+
 int main()
 {
 	pa2m_nuevo_grupo("Pruebas de algo");
--- src/hash.c
+++ src/hash.c
@@ -1,18 +1,78 @@
 #include "hash.h"
 
+#include <stdlib.h>
+
+typedef struct entrada {
+	const char *clave;
+	void *elemento;
+	struct entrada *siguiente;
+} entrada_t;
+
 struct hash {
-	int a;
+	entrada_t **tabla;
+	size_t capacidad;
+	size_t ocupados;
 };
+
 
 hash_t *hash_crear(size_t capacidad)
 {
-	return NULL;
+	hash_t *hash = calloc(1, sizeof(hash_t));
+	hash->capacidad = capacidad;
+
+	entrada_t **tabla = calloc(capacidad, sizeof(entrada_t *));
+	hash->tabla = tabla;
+	
+	return hash;
 }
 
+size_t funcion_hash(const char *clave) {
+	return (size_t)clave[0];
+}
 
-hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
-		      void **anterior)
+// Recibe la cabeza de una nueva lista. A medida que recorra toda la lista, verifica
+// que la clave no coincida.
+// Si no coincide, inserta al final, normal como lista.
+// Si coincide, apunta anterior al elemento. Luego actualiza el elemento al nuevo elemento.
+entrada_t *lista_insertar(entrada_t *head, entrada_t *nueva_entrada, void ***anterior)
 {
+	if (!head) return nueva_entrada;
+
+	entrada_t *iterador = head;
+
+	while (iterador->siguiente != NULL && iterador->clave != nueva_entrada->clave) {
+		iterador = iterador->siguiente;
+	}
+
+	if (!iterador->siguiente) {
+		iterador->siguiente = nueva_entrada;
+		nueva_entrada->siguiente = NULL;
+	}
+
+	if (iterador->clave == nueva_entrada->clave) {
+		anterior = iterador->elemento;
+		iterador->elemento = nueva_entrada->elemento;
+	}
+
+	return head;
+}
+
+hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento, void **anterior)
+{
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+
+	entrada_t *nueva_entrada = malloc(sizeof(entrada_t));
+	nueva_entrada->clave = clave;
+	nueva_entrada->elemento = elemento;
+	nueva_entrada->siguiente = NULL;
+
+	// funcion de lista pero le doy el head de la lista, no la lista.
+	entrada_t *lista = lista_insertar(hash->tabla[posicion], nueva_entrada, &anterior);
+
+	if (!lista) return NULL;
+
+	hash->tabla[posicion] = lista;
+
 	return hash;
 }
 
@@ -45,8 +105,9 @@ void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
 }
 
 size_t hash_con_cada_clave(hash_t *hash,
-			   bool (*f)(const char *clave, void *valor, void *aux),
-			   void *aux)
+						   bool (*f)(const char *clave,
+						   void *valor, void *aux),
+						   void *aux)
 {
 	return 0;
 }
